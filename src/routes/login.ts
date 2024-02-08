f6a84d4e09eebe3196fddd2d9fe30c55c5468ba0
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-08 23:58:35 +0300

    added login routes

---
+++ src/routes/login.ts
@@ -0,0 +1,18 @@
+import express, { Express, Router, Request, Response } from "express";
+
+import { user } from "../data/data";
+
+const router: Router = express.Router()
+
+
+router.post('/api/login', (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  
+    if (email !== user.email || password !== user.password) {
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+  
+    return res.status(200).json({ message: "Login Successful" });
+  });
+
+export default router
